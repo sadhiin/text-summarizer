ad948ce5372d755e0c4e1489ca73707c0ad38fd4
Shanjidul Islam Sadhin <sanjidulislamsadhin1994@gmail.com>
2025-02-01 17:27:32 +0000

    added common utilitis

---
+++ src/textSummarizer/utils/common.py
@@ -0,0 +1,40 @@
+import os
+import yaml
+from pathlib import Path
+from box import ConfigBox
+from ensure import ensure_annotations
+from box.exceptions import BoxValidationError
+from src.textSummarizer.logging import create_logger
+from typing import Any, Dict, List, Union
+
+logger = create_logger(__name__)
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path)->ConfigBox:
+    """
+    Read yaml file and return as ConfigBox object
+    """
+    try:
+        with open(path_to_yaml, 'r') as stream:
+            config = yaml.safe_load(stream)
+            return ConfigBox(config)
+    except BoxValidationError as e:
+        logger.error(f"Error validating yaml file: {e}")
+        return None
+    except Exception as e:
+        logger.error(f"Error reading yaml file: {e}")
+        return None
+
+@ensure_annotations
+def create_directories(dirs: List[Path])->None:
+    """
+    Create directories if they do not exist
+    """
+    try:
+        for dir in dirs:
+            os.makedirs(dir, exist_ok=True)
+            logger.info(f"Directory created: {dir}")
+    
+    except Exception as e:
+        logger.error(f"Error creating directories: {e}")
